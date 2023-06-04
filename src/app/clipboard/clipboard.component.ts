c3740a06fa2c26dde6afb7b1ef60f8149176b1f2
steve <dogshitforreal@gmail.com>
2023-06-04 11:46:49 -0700

    update ui

--- src/app/clipboard/clipboard.component.ts
+++ src/app/clipboard/clipboard.component.ts
@@ -24,6 +24,9 @@ export class ClipboardComponent implements OnInit {
   ngOnInit(): void {
     this.getLatestClipboardContent().subscribe((data) => {
       this.textAreaContent = data.content;
+      this.snackBar.open('Connected', 'Close', {
+        duration: 1000,
+      });
     });
   }
 
