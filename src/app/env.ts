f960a007fcc95c6b560364dde739825ecdea7922
steve <dogshitforreal@gmail.com>
2023-06-06 14:23:14 -0700

    update ui

--- src/app/env.ts
+++ src/app/env.ts
@@ -1,5 +1,5 @@
 export const env = {
   dummyDataMode: false,
-  apiEndPoint: 'https://eb4e-2001-569-7bc2-5000-11cd-c568-d8fc-9a71.ngrok-free.app',
+  apiEndPoint: 'https://5afb-2001-569-7bc2-5000-7985-c189-a6a6-2ca8.ngrok-free.app',
   //apiEndPoint: 'http://127.0.0.1:8000'
 };
