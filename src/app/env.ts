6176da0fa0393acc9803cdfa9b1189064a71da03
steve <dogshitforreal@gmail.com>
2023-06-19 20:04:09 -0700

    fixed dipshit

--- src/app/env.ts
+++ src/app/env.ts
@@ -1,5 +1,5 @@
 export const env = {
   dummyDataMode: false,
-  apiEndPoint: 'https://6571-2001-569-7bc2-5000-9cb9-ea79-5e56-55f8.ngrok-free.app',
-  //apiEndPoint: 'http://127.0.0.1:8000'
+  apiEndPoint: 'https://be7f-2001-569-7bc2-5000-c555-b4e3-a51c-a7c7.ngrok-free.app/homeProj',
+  //apiEndPoint: 'http://127.0.0.1:8000/homeProj'
 };
