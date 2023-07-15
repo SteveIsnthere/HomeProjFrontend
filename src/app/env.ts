fba697acfd74c48b5a35af4e8c2f4e38a0fb7c95
priva <dogshitforreal@gmail.com>
2023-07-15 01:52:58 -0700

    added reload functionallity

--- src/app/env.ts
+++ src/app/env.ts
@@ -1,5 +1,5 @@
 export const env = {
   dummyDataMode: false,
-  apiEndPoint: 'https://be7f-2001-569-7bc2-5000-c555-b4e3-a51c-a7c7.ngrok-free.app/homeProj',
+  apiEndPoint: 'https://c490-2001-569-58aa-1400-f9bc-a02c-174-ae1b.ngrok-free.app/homeProj',
   //apiEndPoint: 'http://127.0.0.1:8000/homeProj'
 };
