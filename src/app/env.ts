4b5bfe92a7f263f42d18a431e820eae14a880d59
priva <npminitfailed@gmail.com>
2023-08-09 10:23:36 -0700

    fixed dipshit

--- src/app/env.ts
+++ src/app/env.ts
@@ -1,5 +1,5 @@
 export const env = {
   dummyDataMode: false,
-  apiEndPoint: 'https://be7f-2001-569-7bc2-5000-c555-b4e3-a51c-a7c7.ngrok-free.app/homeProj',
+  apiEndPoint: 'https://steveapi.ngrok.io/homeProj',
   //apiEndPoint: 'http://127.0.0.1:8000/homeProj'
 };
