2dc4915254cdcbfe574576b5adddb45b32720dbc
Steve Wang <npminitfailed@gmail.com>
2025-02-16 20:08:32 -0800

    fixes

--- src/app/env.ts
+++ src/app/env.ts
@@ -1,5 +1,5 @@
 export const env = {
   dummyDataMode: false,
-  apiEndPoint: 'https://dogshit.ngrok.app/homeProj',
+  apiEndPoint: 'https://by-steve.ngrok.io/homeProj',
   //apiEndPoint: 'http://127.0.0.1:8000/homeProj'
 };
